4dc9e4bb7b91fd4a63dbf4d143f958cc16c968cf
bockdavidson <dbock1994@gmail.com>
2018-12-28 10:57:01 -0500

    adding front end of project 3

---
+++ Project 3/Front-End/toolbar.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AuditService } from 'src/app/Audit/Services/audit.service';
+import { Batch } from 'src/app/Batch/type/batch';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+  batches: Batch[];
+  weeks = [1,2,3,4,5,6];
+  selectedWeek: number;
+
+  constructor() { }
+
+  ngOnInit() {   
+    this.selectedWeek=6;
+  }
+
+  showActiveWeek(week: number) {
+    if (week==this.selectedWeek) {
+      return "active";
+    }
+  }
+
+  selectWeek(event: number) {
+    this.selectedWeek = event;
+  }
+
+  addWeek() {
+    var last = this.weeks[this.weeks.length-1];
+    this.weeks.push(last+1);
+    this.selectedWeek=last+1;
+  }
+
+  getWeeks() {
+    console.log(this.batches[0].weeks);
+  }
+
+}
